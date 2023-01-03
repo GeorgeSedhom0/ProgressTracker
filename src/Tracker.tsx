58c0de522140d74848a63236f94b72dec8cd9d20
GeorgeSedhom0 <george.facebooka@gmail.com>
2023-01-03 22:35:02 +0200

    creating init tracker

--- src/Tracker.tsx
+++ src/Tracker.tsx
@@ -1,12 +1,51 @@
 // this projecy will be GitHub like Progress Tracker
+import { useState } from "react";
 
-const Tracker = () => {
-    return (
-        <div>
-        <h1>Tracker</h1>
-        </div>
-    );
-    
+interface Progress {
+  progress: number;
+  date: number;
 }
+const Tracker = () => {
+  const [progress, setProgress] = useState<Progress[]>([
+    {
+      progress: 0,
+      date: Date.now(),
+    },
+  ]);
+
+  const colorByProgress = [
+    "darkgray",
+    "gray",
+    "lightgreen",
+    "green",
+    "darkgreen",
+  ];
+
+  // create mock progress data
+  return (
+    <div>
+      <h1>Tracker</h1>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          width: "100%",
+        }}
+      >
+        {progress.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="progressSquare"
+              style={{
+                backgroundColor: colorByProgress[item.progress],
+              }}
+            ></div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Tracker;
