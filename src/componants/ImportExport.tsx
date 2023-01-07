d60f2c026ce35e4bbc9252a385d9dc9abaa3a9f1
GeorgeSedhom0 <george.facebooka@gmail.com>
2023-01-07 22:24:29 +0200

    functional backup seperate button

--- src/componants/ImportExport.tsx
+++ src/componants/ImportExport.tsx
@@ -14,10 +14,34 @@ const ImportExport = () => {
       <Button
         variant="contained"
         onClick={() => {
-          console.log("import");
+          // get the data from localStoarge
+          const savedData = localStorage.getItem("progress");
+          if (!savedData) {
+            alert("No data to backup");
+          } else {
+            const data = JSON.parse(savedData);
+            // save the data to a file
+            const blob = new Blob([JSON.stringify(data)], {
+              type: "application/json",
+            });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.download = "progress.json";
+            a.href = url;
+            a.click();
+            a.remove();
+          }
         }}
       >
-        Backup / Restore
+        Backup
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => {
+          //
+        }}
+      >
+        Restore
       </Button>
     </div>
   );
